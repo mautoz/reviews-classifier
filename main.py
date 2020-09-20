16b9cd7cff1b682e0a02bcea1380cfefe6194083
Mauricio Tavares Ozaki <ozaki@versatushpc.com.br>
2020-09-20 14:09:59 -0300

    Continuar....

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from helpers import db_aux
 def start():
     try:
         with db_aux.connect_db as conn:
+    
+    except Exception as e:
+        print(e)
              
 
 if __name__ == "__main__":
